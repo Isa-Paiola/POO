da22b1887fa51d2ef0ecbdf15b862789e77f3554
Isa-Paiola <isabelapaiola6@gmail.com>
2025-03-24 15:52:34 -0300

    Create abstracao.php

---
+++ abstracao.php
@@ -0,0 +1,19 @@
+<?php
+
+// Classe base abstrata
+abstract class Pessoa {
+    public $nome;
+
+    public function __construct($nome) {
+        $this->nome = $nome;
+    }
+}
+
+// Classe Aluno herda de Pessoa
+class Aluno extends Pessoa {}
+
+// Criando objeto
+$rafael = new Aluno("Rafael");
+echo $rafael->nome . "\n"; // Rafael
+
+?>
